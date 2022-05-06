1104fec20207353290413d279639f13a2d902fa4
Asvprocky <104539478+Asvprocky@users.noreply.github.com>
2022-05-06 16:57:37 +0900

    Add files via upload

---
+++ jqrtest.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+    $("#awge1").click(function () {
+        $("#awge1").slideUp(650);
+    });
+});
+
+$(document).ready(function () {
+    $("#awge2").click(function () {
+        $("#awge2").slideUp(650);
+    });
+});
+
+$(document).ready(function () {
+    $("#awge3").click(function () {
+        $("#awge3").slideUp(650);
+    });
+});
+$(document).ready(function () {
+    $("#awge4").click(function () {
+        $("#awge4").slideUp(650);
+    });
+});
+
+$(document).ready(function () {
+    $("#awge6").click(function () {
+        $("#awge6").slideUp(650);
+    });
+});
+
+
+
+//$(document).ready(function () {
+//    $("#movie_list").click(function () {
+//        $("#movie_list ul li").hide(650);
+//    });
+//});
+$(document).ready(function () {
+    $("#awge5").click(function () {
+        $("#hi2,#hi3,#hi1,#awge1,#awge2,#awge3,#awge4,#awge6").show(650);
+    });
+});
+
+$(document).ready(function () {
+    $("#hi1").click(function () {
+        $("#hi1").hide(650);
+    });
+});
+
+$(document).ready(function () {
+    $("#hi2").click(function () {
+        $("#hi2").hide(650);
+    });
+});
+
+$(document).ready(function () {
+    $("#hi3").click(function () {
+        $("#hi3").hide(650);
+    });
+});
+
+$(document).ready(function () {
+    $("#btn33").click(function () {
+        $("#re2").remove();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn22").click(function () {
+        $("#awge1,#awge2,#awge3,#awge4,#awge6,ps").remove();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn44").click(function () {
+        $("#awge1,#awge2,#awge3,#awge4,#awge6,").append();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn1").click(function () {
+        $("p").append(" <b>D.M.B.</b>.");
+    });
+
+    $("#btn2").click(function () {
+        $("#movie_list").append("<li>I DO</li>");
+    });
+});
